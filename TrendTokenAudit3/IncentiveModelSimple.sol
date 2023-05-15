6ce5e00f2587c47a854bdcaf91cf1378bd1e27ff
JavisJL <38714211+JavisJL@users.noreply.github.com>
2023-05-15 17:46:31 -0600

    Update IncentiveModelSimple.sol
    - allows admin to adjust fees without requiring to redeploy a new contract and connect to Trend Token

--- TrendTokenAudit3/IncentiveModelSimple.sol
+++ TrendTokenAudit3/IncentiveModelSimple.sol
@@ -7,8 +7,11 @@ import "./Lib.sol";
 import "./SignedSafeMath.sol";
 import "./SafeMath.sol";
 
-
-
+// Add to Github Repo
+// 1) Create updatable feePerToken() underlying:fee pairs, similar to CompTT (make sure cant duplicate underlying)
+//      - add array of underlying
+// 2) Upgradeable protocolFeeDeposit, protocolFeeRedeem, protocolFeeTrade
+//      - keep discount and threshold fixed 
 
 contract IncentiveModelSimple is IIncentiveModelSimple {
 
@@ -17,56 +20,126 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
 
     // ---------- PUBLIC VARIABLES ------------- // 
 
+    /**
+     * @notice Used as a sanity check when adding incentive model to Trend Token
+     */
+    bool public constant isIncentiveModel = true;
 
-    bool public isIncentiveModel = true;
-
+    /**
+     * @notice Has permission to make changes
+     */
     address public admin;
 
     /**
      *@notice Base fee charged when depositing for Trend Tokens
      */
-    uint public constant protocolFeeDeposit = 0.0025e18;
+    uint public protocolFeeDeposit = 0.0015e18;
 
     /**
-     *@notice Base fee charged when redeeming Trend Tokens or redeeming Trend Tokens
+     *@notice Base fee charged when redeeming Trend Tokens for underlying
      */
-    uint public constant protocolFeeRedeem = 0.0050e18;
+    uint public protocolFeeRedeem = 0.0025e18;
 
     /**
-     *@notice Base fee charged when redeeming Trend Tokens or redeeming Trend Tokens
+     *@notice Base fee charged when trading underlying for underlying
      */
-    uint public constant protocolFeeTrade = 0.0050e18;
+    uint public protocolFeeTrade = 0.0015e18;
     
 
+    /**
+     * @notice Discounts when meeting low, med, and high XTT Thresholds
+     */
     uint public lowDiscount = 0.20e18;
     uint public medDiscount = 0.40e18;
-    uint public highDiscount = 0.60e18;
+    uint public highDiscount = 0.80e18;
 
+    /**
+     * @notice Amount of XTT required to be held to receive low, med, and high discounts
+     */
     uint public lowThreshold = 100e18;
     uint public medThreshold = 10000e18;
     uint public highThreshold = 100000e18;
 
 
     /**
-     * @notice allocations can be +/- this percentage before deposit/redeem incentive or reward
-     * @dev Allocation Delta will trigger a fee/reward if [-5%, 5%] if threshold = 5%
+     * @notice Maps ERC20 token to its base reward/fee when depositing or redeeming
+     * @dev If BNB: 0.50%, then 0.50% reward when depositing BNB if protocol desires more
+     *                          0.50% fee when redeeming BNB if protocol desires less
      */
-    uint public threshold = 0.05e18;
+    mapping(address => uint) public feePerToken;
+
+
+    /**
+     * @notice Maintains a list of all underlying assets that are included in the feePerToken mapping
+     *         This allows this underlying to be deposited, redeemed, or traded
+     */
+    IERC20[] public allUnderlying;
+
+
+    /**
+     * @notice Emitted when updated associated values
+     */
+    event UpdateFeeDiscount(uint oldLow, uint oldMed, uint oldHigh, uint newLow, uint newMed, uint newHigh);
+    event UpdateDepositFee(uint oldFee, uint newFee);
+    event UpdateRedeemFee(uint oldFee, uint newFee);
+    event UpdateProtocolFee(uint oldFee, uint newFee);
+
+
 
     constructor() public {
         admin = msg.sender;
     }
 
-
+    
     /**
      *  Changes the XTT holding threshold for 20%, 40%, and 60% trading fees on buying/selling Trend Tokens
      */
     function _updateTradeFeeDiscounts(uint _lowThres, uint _medThres, uint _highThres) external {
         require(msg.sender == admin,"!admin");
-        require(_lowThres < _medThres && _medThres < _highThres,"!threshold");
+        require(_lowThres < _medThres && _medThres < _highThres && _highThres <= 1e18,"!threshold");
+        uint oldLow = lowThreshold;
+        uint oldMed = medThreshold;
+        uint oldHigh = highThreshold;
         lowThreshold = _lowThres;
         medThreshold = _medThres;
         highThreshold = _highThres;
+        emit UpdateFeeDiscount(oldLow, oldMed, oldHigh, lowThreshold, medThreshold, highThreshold);
+    }
+
+
+    /**
+     * @notice Updates deposit fee when buying Trend Tokens
+     */
+    function _updateDepositFee(uint _newDepositFee) external {
+        require(msg.sender == admin,"!admin");
+        require(_newDepositFee <= 0.05e18,"max 5%");
+        uint oldFee = protocolFeeDeposit;
+        protocolFeeDeposit = _newDepositFee;
+        emit UpdateDepositFee(oldFee, protocolFeeDeposit);
+    }
+
+
+    /**
+     * @notice Updates redeem fee when selling Trend Tokens
+     */
+    function _updateRedeemFee(uint _newRedeemFee) external {
+        require(msg.sender == admin,"!admin");
+        require(_newRedeemFee <= 0.10e18,"max 10%");
+        uint oldFee = protocolFeeRedeem;
+        protocolFeeRedeem = _newRedeemFee;
+        emit UpdateRedeemFee(oldFee, protocolFeeRedeem);
+    }
+
+
+    /**
+     * @notice Updates redeem fee when trading underlying for underlying
+     */
+    function _updateProtocolFeeTrade(uint _newProtocolFee) external {
+        require(msg.sender == admin,"!admin");
+        require(_newProtocolFee <= 0.050e18,"max 5%");
+        uint oldFee = protocolFeeTrade;
+        protocolFeeTrade = _newProtocolFee;
+        emit UpdateProtocolFee(oldFee, protocolFeeTrade);
     }
 
 
@@ -90,7 +163,7 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
     /** CHANGE TO MAINNET ADDRESSES
      * @notice Returns the base trading fee based on what token is deposited or redeemed
      * @dev Higher base trading fee if token has lower liquidity to estimate higher slippage
-     */
+     
     function feePerToken(IERC20 _bep20) public pure returns(uint) {
         if (_bep20 == IERC20(0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd)) {
             return 0.0020e18;
@@ -102,8 +175,52 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
             return 0.0050e18;
         }
     }
+    */
 
 
+    /**
+     * @notice Returns true if underlying is supported
+     */
+    function underlyingSupported(IERC20 underlying) internal view returns(bool) {
+        for (uint i = 0; i < allUnderlying.length; i ++) {
+            if (allUnderlying[i] == underlying) {
+                return true;
+            }
+        }
+        return false;
+    } 
+
+
+    /**
+     * @notice Allows admin to change the fee/reward for deposit, redeem, and trade for each token
+     * @ testnet
+     * BNB: 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd ==> 2000000000000000
+     * BUSD: 0x8301F2213c0eeD49a7E28Ae4c3e91722919B8B47 ==> 2500000000000000
+     * BTC: 0xA808e341e8e723DC6BA0Bb5204Bafc2330d7B8e4 ==> 3000000000000000
+     */
+    function _updateFeePerToken(IERC20 underlying, uint feeOrReward) external {
+        require(msg.sender == admin,"!admin");
+        require(feeOrReward > 0 && feeOrReward <= 0.05e18,"max 5%, min>0");
+        if (!underlyingSupported(underlying)) {
+            allUnderlying.push(underlying);
+        }
+        feePerToken[address(underlying)] = feeOrReward;
+    }
+
+
+    /**
+     * @notice Returns the fee/reward for specified underlying token
+     * @dev If fee/reward is zero, then it wasnt updated, and default fee 5%
+     */
+    function returnFeePerToken(IERC20 underlying) internal view returns(uint) {
+        uint fee = feePerToken[address(underlying)];
+        require(fee>0,"feePerToken not updated.");
+        return fee;
+    }
+
+    function returnFeePerTokenExt(IERC20 underlying) external view returns(uint) {
+        return returnFeePerToken(underlying);
+    }
 
     // ------------- CALCULATING DEPOSIT/REDEEM FEE OR REWARD ---------------- //
 
@@ -115,14 +232,14 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
      * @param _priorDelta Desired allocations minus actual allocations before _bep20 deposit (positive if desired more asset) 
      * @param _postDelta Desired allocations minus estimated allocations after _bep20 deposit
      */
-    function depositRewardOrFee(IERC20 _bep20, uint _valueIn, int _priorDelta, int _postDelta) public pure returns(uint reward, uint fee) {
+    function depositRewardOrFee(IERC20 _bep20, uint _valueIn, int _priorDelta, int _postDelta) internal view returns(uint reward, uint fee) {
 
         _valueIn; // may be used in the future to vary deposit reward (or fee)
 
         if (_priorDelta >= 0) { // require more of the token (net reward unless far excess) 
 
             uint priorDelta = uint(_priorDelta);
-            reward = feePerToken(_bep20);
+            reward = returnFeePerToken(_bep20);
 
             if (_postDelta < 0) { // all deposit goes towards desired
 
@@ -145,7 +262,7 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
 
         } else { // desires less of the token (full fee)
 
-            fee = feePerToken(_bep20);
+            fee = returnFeePerToken(_bep20);
 
         }
 
@@ -153,18 +270,18 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
     }
 
 
-    function redeemRewardOrFee(IERC20 _bep20, uint _valueIn, int _priorDelta, int _postDelta) public pure returns(uint reward, uint fee) {
+    function redeemRewardOrFee(IERC20 _bep20, uint _valueIn, int _priorDelta, int _postDelta) internal view returns(uint reward, uint fee) {
 
         _valueIn; // may be used in the future to vary deposit reward (or fee)
         
         if (_priorDelta <= 0) { // require less of the token (net reward unless far excess) 
 
-            reward = feePerToken(_bep20);
+            reward = returnFeePerToken(_bep20);
 
-            if (_postDelta < 0) { // all deposit goes towards desired
+            if (_postDelta > 0) { // all deposit goes towards desired
 
                 uint priorDelta = uint(-_priorDelta);
-                uint excess = uint(-_postDelta);
+                uint excess = uint(_postDelta);
 
                 if (priorDelta >= excess) { // most of the deposit goes towards desired (net reward) 
 
@@ -183,7 +300,7 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
 
         } else { // desires less of the token (full fee)
 
-            fee = feePerToken(_bep20);
+            fee = returnFeePerToken(_bep20);
 
         }
 
@@ -212,7 +329,7 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
      * @param fee The incentive fee if any
      */
     function totalDepositFee(IERC20 _depositBEP20, uint _valueDeposit, int priorDelta, int postDelta, uint price, uint balanceXTT)  
-             public view returns(int totalFee, uint protocolFeePerc, uint reward, uint fee) {
+             external view returns(int totalFee, uint protocolFeePerc, uint reward, uint fee) {
         price; // may be used in the future
         (reward, fee) = depositRewardOrFee(_depositBEP20,_valueDeposit, priorDelta, postDelta);
         uint feeFactorFromDiscountXTT = uint(1e18).sub(feeDiscount(balanceXTT));
@@ -223,17 +340,6 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
     }
 
 
-    // assume BUSD = 100%, currently at 90% ($90 BUSD, $10 BNB)
-    // inputs,
-    // valueDeposited = $50
-    // allocationDelta = 10%
-
-    // would like full reward ==> but thinks only $10 of the $50 is deposited, so gives full fee instead of reward 
-
-    // needs to compare the allocation before and allocation after
-    // compare the two, and give a discount if closer than desired? 
-
-
     /** 
      * @notice Returns the total Trend Token redeem fee 
      * @dev May be negative if reward exceeds the protocolBaseFee (especially if high XDP discount)
@@ -241,7 +347,7 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
      * @param fee The incentive fee if any
      */
     function totalRedeemFee(IERC20 _redeemBep20, uint _valueRedeem, int priorDelta, int postDelta, uint price, uint balanceXTT) 
-             public view returns(int totalFee, uint protocolFeePerc, uint reward, uint fee) {
+             external view returns(int totalFee, uint protocolFeePerc, uint reward, uint fee) {
         price; // may be used in the future
         (reward, fee) = redeemRewardOrFee(_redeemBep20,_valueRedeem, priorDelta, postDelta);
         uint feeFactorFromDiscountXTT = uint(1e18).sub(feeDiscount(balanceXTT));
@@ -250,6 +356,8 @@ contract IncentiveModelSimple is IIncentiveModelSimple {
         return (totalFee, protocolFeePerc, reward, fee);
 
     }
+
+    // ------------- TRADE FUNCTIONS ----------------- // 
 
 
     /**
